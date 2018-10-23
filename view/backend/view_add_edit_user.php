c407a2377c6700af81818d359517dd62e6cd222f
thamnt123 <32872947+thamnt123@users.noreply.github.com>
2018-10-23 22:21:01 +0700

    2

--- view/backend/view_add_edit_user.php
+++ view/backend/view_add_edit_user.php
@@ -1,53 +1,76 @@
-<div class="row justify-content-center">
-	<div class="col-md-12">
-		<!-- card -->		
-		<div class="card  border-primary">
-			<div class="card card-header bg-primary text-white">Add edit user</div>
-			<div class="card-body">
-				<form method="post" action="<?php echo $form_action; ?>">
-					<!-- form group -->
-					<div class="form-group">
-						<div class="row">
-							<div class="col-md-2 text-right">Full name</div>
-							<div class="col-md-10">
-								<input type="text" name="c_fullname" value="<?php echo isset($record->c_fullname)?$record->c_fullname:''; ?>" class="form-control">
-							</div>
-						</div>
-					</div>
-					<!-- end form group -->
-					<!-- form group -->
-					<div class="form-group">
-						<div class="row">
-							<div class="col-md-2 text-right">Email</div>
-							<div class="col-md-10">
-								<input <?php if(isset($record->c_email)): ?> disabled <?php else: ?> required <?php endif; ?> type="email" name="c_email" value="" class="form-control">
-							</div>
-						</div>
-					</div>
-					<!-- end form group -->
-					<!-- form group -->
-					<div class="form-group">
-						<div class="row">
-							<div class="col-md-2 text-right">Password</div>
-							<div class="col-md-10">
-<input type="password" name="c_password" <?php if(isset($record->c_email)): ?> placeholder="Nhập password mới nếu muốn đổi password" <?php else: ?> required <?php endif; ?>  class="form-control">
-							</div>
-						</div>
-					</div>
-					<!-- end form group -->
-					<!-- form group -->
-					<div class="form-group">
-						<div class="row">
-							<div class="col-md-2 text-right"></div>
-							<div class="col-md-10">
-								<input type="submit" value="Process" class="btn btn-primary"> <input type="reset" value="Reset" class="btn btn-danger">
-							</div>
-						</div>
-					</div>
-					<!-- end form group -->
-				</form>
-			</div>
-		</div>
-		<!-- end card -->
-	</div>
-</div>
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Thêm người dùng</h3>
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_content">
+                    <br />
+                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="post" enctype="multipart/form-data" action="<?php echo $form_action; ?>">
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" >Họ và tên <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        	<input type="text" name="c_fullname" value="<?php echo isset($record->c_fullname)?$record->c_fullname:''; ?>" required class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Email <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        	<input type="email" name="c_email" value="" <?php if(isset($record->c_email)): ?> disabled <?php else: ?> required <?php endif; ?> class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Password <span class="required">*</span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        	<input type="password" name="c_password" <?php if(isset($record->c_email)): ?> placeholder="Nhập password mới nếu muốn đổi password" <?php else: ?> required <?php endif; ?> class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button class="btn btn-primary" type="button">Cancel</button>
+						              <button class="btn btn-primary" type="reset" value="Reset">Reset</button>
+                          <button type="submit" value="Process" class="btn btn-success">Submit</button>
+                        </div>
+                      </div>
+
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+
+
+
--- view/backend/view_user.php
+++ view/backend/view_user.php
@@ -1,42 +1,88 @@
-<div class="row justify-content-center">
-	<div class="col-md-12">
-		<!-- card -->
-		<div style="margin:15px 0px; ">
-			<a href="admin.php?controller=add_edit_user&act=add" class="btn btn-primary">Add</a>
-		</div>
-		<div class="card border-primary">
-			<div class="card card-header bg-primary text-white" style="padding:7px !important;">Login</div>
-				<div class="card-body">
-				<!-- table -->
-				<table class="table table-hover table-bordered">
-					<tr>
-						<th>Họ và tên</th>
-						<th>Email</th>
-						<th style="width: 150px;"></th>
-					</tr>
-				<?php foreach($arr as $rows): ?>
-					<tr>
-						<td><?php echo $rows->c_fullname; ?></td>
-						<td><?php echo $rows->c_email; ?></td>
-						<td style="text-align: center;">
-							<a href="admin.php?controller=add_edit_user&act=edit&id=<?php echo $rows->pk_user_id; ?>">Edit</a>&nbsp;&nbsp;
-							<a onclick="return window.confirm('Are you sure?');" href="admin.php?controller=add_edit_user&act=delete&id=<?php echo $rows->pk_user_id; ?>">Delete</a>
-						</td>
-					</tr>
-				<?php endforeach; ?>
-				</table>
-				<!-- end table -->
-				</div>
-				<div class="card-footer" style="padding:7px !important">
+  
+ <!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Quản lý người dùng</h3>
+      </div>
+
+      <div class="title_right">
+        <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="button">Go!</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <div class="clearfix"></div>
+
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <div>
+				<a href="admin.php?controller=add_edit_user&act=add" class="btn btn-primary">Add</a>
+			</div>
+            <div class="clearfix"></div>
+          </div>
+
+          <div class="x_content">
+            <div class="table-responsive">
+              <table class="table table-striped jambo_table bulk_action">
+                <thead>
+                  <tr class="headings">
+                    <th>
+                      <input type="checkbox" id="check-all" class="flat">
+                    </th>
+                    <th class="column-title">Họ và tên </th>
+                    <th class="column-title">Email </th>
+               
+                    <th class="column-title no-link last" ><span class="nobr">Action</span>
+                    </th>
+                    <th class="bulk-actions" colspan="7">
+                      <a class="antoo" style="color:#fff; font-weight:500;">Bulk Actions ( <span class="action-cnt"> </span> ) <i class="fa fa-chevron-down"></i></a>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php foreach($arr as $rows): ?>
+                  <tr class="even pointer">
+                    <td class="a-center ">
+                      <input type="checkbox" class="flat" name="table_records">
+                    </td>
+                    <td class=" "><?php echo $rows->c_fullname; ?></td>
+                    <td class=" "><?php echo $rows->c_email; ?></td>
+                    <td class=" last">
+                    	<a href="admin.php?controller=add_edit_user&act=edit&id=<?php echo $rows->pk_user_id; ?>">Edit</a>&nbsp;&nbsp;
+						<a onclick="return window.confirm('Are you sure?');" href="admin.php?controller=add_edit_user&act=delete&id=<?php echo $rows->pk_user_id; ?>">Delete</a>
+                    </td>
+                  </tr>
+                <?php endforeach; ?>
+                </tbody>
+              </table>
+
+            <div>
+				<a href="#" class="btn btn-primary">Delete</a>
+			</div>
+              <!-- phân trang -->
+	          	<div class="card-footer" style="padding:5px !important">
 					<ul class="pagination">
 						<li class="page-item"><a class="page-link" href="#">Trang</a></li>
-					<?php for($i = 1; $i <= $num_page; $i++): ?>	
+					<?php for($i=1; $i<=$num_page; $i++): ?>	
 						<li class="page-item"><a class="page-link" href="admin.php?controller=user&p=<?php echo $i; ?>"><?php echo $i; ?></a></li>
-					<?php endfor; ?>	
+					<?php endfor; ?>
 					</ul>
 				</div>
-			</div>
-		</div>
-		<!-- end card -->
-	</div>
+			<!-- end phân trang -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+<!-- /page content -->
